5018b33abe368512200a56f20c2d33c509cf29be
Sehwa Ryu <sehwa_ryu@berkeley.edu>
2020-07-04 15:59:07 +0900

    Fix Merge Conflict

--- TutorDot_iOS_1/TutorDot_iOS/AppDelegate.swift
+++ TutorDot_iOS_1/TutorDot_iOS/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import DropDown
+//import DropDown
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        DropDown.startListeningToKeyboard()
+        //DropDown.startListeningToKeyboard()
         return true
     }
 
