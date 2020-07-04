4f89d049fefc458e32aef5dea49c727fcaab00bd
Sehwa Ryu <sehwa_ryu@berkeley.edu>
2020-07-04 13:06:52 +0900

    Add ClassInfoVC

---
+++ TutorDot_iOS_1/TutorDot_iOS/Cells/CalendarCell.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarCell.swift
+//  TutorDot_iOS
+//
+//  Created by Sehwa Ryu on 03/07/2020.
+//  Copyright © 2020 Sehwa Ryu. All rights reserved.
+//
+
+import Foundation
---
+++ TutorDot_iOS_1/TutorDot_iOS/VC/ClassInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  ClassInfoVC.swift
+//  TutorDot_iOS
+//
+//  Created by Sehwa Ryu on 03/07/2020.
+//  Copyright © 2020 Sehwa Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ClassInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
