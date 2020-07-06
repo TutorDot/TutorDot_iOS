1d6b6e5acb89500eed8c08896d9497d18af3beed
Sehwa Ryu <sehwa_ryu@berkeley.edu>
2020-07-06 16:42:18 +0900

    Update Alert.storyboard

--- TutorDot_iOS_1/TutorDot_iOS/VC/AlertVC.swift
+++ TutorDot_iOS_1/TutorDot_iOS/VC/AlertVC.swift
@@ -10,21 +10,52 @@ import UIKit
 
 class AlertVC: UIViewController {
 
+    @IBOutlet weak var noticeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpTableView()
+        noticeTableView.register(UINib(nibName:"AlertTableViewCell", bundle: nil), forCellReuseIdentifier: "AlertTableViewCell")
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func setUpTableView() {
+        noticeTableView.delegate = self
+        noticeTableView.dataSource = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AlertVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: AlertTableViewCell = tableView.dequeueReusableCell(withIdentifier: AlertTableViewCell.identifier, for: indexPath) as! AlertTableViewCell
+        cell.layer.cornerRadius = 8
+        cell.layer.borderColor = UIColor.clear.cgColor
+        cell.layer.borderWidth = 3
+        return cell
+    }
+    
+    // Header 설정
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "5월 29일"
+    }
+    
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let headerView = UIView()
+//        headerView.backgroundColor = UIColor.clear
+//        return headerView
+//    }
+    
+    
+    
 }
