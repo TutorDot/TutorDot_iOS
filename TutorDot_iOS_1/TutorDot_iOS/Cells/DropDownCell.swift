325bcda9b4dd38a4640da82543d4e510553e364b
Sehwa <>
2020-07-07 14:51:12 +0900

    Update Calendar_branch

--- TutorDot_iOS_1/TutorDot_iOS/Cells/DropDownCell.swift
+++ TutorDot_iOS_1/TutorDot_iOS/Cells/DropDownCell.swift
@@ -6,19 +6,19 @@
 //  Copyright © 2020 Sehwa Ryu. All rights reserved.
 //
 
-import UIKit
-
-class DropDownCell: DropDownCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-}
+//import UIKit
+//
+//class DropDownCell: DropDownCell {
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
