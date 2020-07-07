a9d28c4d08dc18749f76a8f8c3919750e737898f
indoni <choinj97@gmail.com>
2020-07-07 22:11:21 +0900

    Update journal view button action

--- TutorDot_iOS_1/TutorDot_iOS/Cells/JournalDataCell.swift
+++ TutorDot_iOS_1/TutorDot_iOS/Cells/JournalDataCell.swift
@@ -21,6 +21,9 @@ class JournalDataCell: UITableViewCell {
     @IBOutlet weak var hwInprogressButton: UIButton!
     @IBOutlet weak var hwCompleteButton: UIButton!
     
+    var iscompleteBtn: Bool = false
+    var isIncompleteBtn: Bool = false
+    var isInprogress: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -39,16 +42,68 @@ class JournalDataCell: UITableViewCell {
         homeworkLabel.text = "숙제 : " + homework
     }
     
+    func setcompleteBtn(_ status: Bool){
+        if status {
+            hwCompleteButton.setImage(UIImage(named: "classLogModificationBtnCirclePick"), for: .normal)
+            iscompleteBtn = true
+        } else {
+            hwCompleteButton.setImage(UIImage(named: "classLogModificationBtnCircleUnpick"), for: .normal)
+            iscompleteBtn = false
+        }
+    }
+    
+    func setIncompleteBtn(_ status: Bool){
+        if status {
+            hwIncompleteButton.setImage(UIImage(named: "classLogModificationBtnXPick"), for: .normal)
+            isIncompleteBtn = true
+        } else {
+            hwIncompleteButton.setImage(UIImage(named: "classLogModificationBtnXUnpick"), for: .normal)
+            isIncompleteBtn = false
+        }
+    }
 
+    func setInprogressBtn(_ status: Bool){
+        if status {
+           hwInprogressButton.setImage(UIImage(named: "classLogModificationBtnTrianglePick"), for: .normal)
+           isInprogress = true
+        } else {
+           hwInprogressButton.setImage(UIImage(named: "classLogModificationBtnTriangleUnpick"), for: .normal)
+           isInprogress = false
+        }
+    }
+    
     @IBAction func pressCompleteBtn(_ sender: Any) {
-        
+        print("complete")
+        if !iscompleteBtn {
+            setcompleteBtn(true)
+            setInprogressBtn(false)
+            setIncompleteBtn(false)
+        } else {
+            setcompleteBtn(false)
+        }
     }
     
     @IBAction func pressInprogressBtn(_ sender: Any) {
-        
+        print("Inprogress")
+        if !isInprogress {
+            setcompleteBtn(false)
+            setInprogressBtn(true)
+            setIncompleteBtn(false)
+        } else {
+            setInprogressBtn(false)
+        }
     }
     
     @IBAction func pressIncompleteBtn(_ sender: Any) {
-        
+        print("Incomplete")
+        if !isIncompleteBtn {
+            setcompleteBtn(false)
+            setInprogressBtn(false)
+            setIncompleteBtn(true)
+        } else {
+            setIncompleteBtn(false)
+        }
     }
+    
+    
 }
