2f1e497b5b94488daa697bfe8c91fdae362da98c
indoni <choinj97@gmail.com>
2020-07-12 10:14:17 +0900

    Update NotesModifyVC 뷰 업데이트

--- TutorDot_iOS_1/TutorDot_iOS/Source/VCs/NotesModifyVC.swift
+++ TutorDot_iOS_1/TutorDot_iOS/Source/VCs/NotesModifyVC.swift
@@ -82,15 +82,10 @@ class NotesModifyVC: UIViewController {
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonDidTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+
 
 }
