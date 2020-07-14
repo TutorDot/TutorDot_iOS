c1c894a0064143218a573c13b95d7ba1a44568f0
indoni <choinj97@gmail.com>
2020-07-15 05:49:03 +0900

    Add mypage new View - 수업정보조회

---
+++ TutorDot_iOS_1/TutorDot_iOS/Source/Cells/ClassConnectCell.swift
@@ -0,0 +1,25 @@
+//
+//  ClassConnectCell.swift
+//  TutorDot_iOS
+//
+//  Created by 최인정 on 2020/07/15.
+//  Copyright © 2020 Sehwa Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ClassConnectCell: UITableViewCell {
+
+    static let identifier: String = "ClassConnectCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
